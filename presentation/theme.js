89b1a5d756bf764ee445c2274933ae2cae2acb1d
Bryce Covert <bryce@pelo.tech>
2019-03-26 17:27:26 -0700

    Adding presentation

---
+++ presentation/theme.js
@@ -0,0 +1,14 @@
+import { swiss }from 'mdx-deck/themes'
+import { syntaxHighlighterPrism }from 'mdx-deck/themes'
+import {
+    future
+} from 'mdx-deck/themes'
+
+// import style from 'react-syntax-highlighter/dist/styles/prism/okaidia';
+// import javascript from 'react-syntax-highlighter/dist/languages/prism/javascript'
+
+export default {
+    // extends the default theme
+    ...swiss,
+    
+}
